681876307b0aa26b36a9b768892f95a9de622c88
Hisean Valdez <hisean.john.valdez@sunlife.com>
2022-08-08 18:07:39 +0800

    Styling updates. Add render method.

--- tooltip.js
+++ tooltip.js
@@ -1,44 +1,117 @@
 class Tooltip extends HTMLElement {
-    constructor() {
-      super();
-      this._tooltipContainer;
-      this._tooltipText = 'Some dummy tooltip text.';
-      this.attachShadow({ mode: 'open' });
-      this.shadowRoot.innerHTML = `
-          <style>
-              div {
-                  background-color: black;
-                  color: white;
-                  position: absolute;
-                  z-index: 10;
-              }
-          </style>
-          <slot>Some default</slot>
-          <span> (?)</span>
+  constructor() {
+    super();
+
+    this._tooltipIcon;
+    this._tooltipText = "Some dummy tooltip text.";
+    this._tooltipVisible = false;
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+        <style>
+          div {
+            font-weight: normal;
+            background-color: black;
+            color: white;
+            position: absolute;
+            top: 1.5rem;
+            left: 0.75rem;
+            z-index: 10;
+            padding: 0.15rem;
+            border-radius: 3px;
+            box-shadow: 1px 1px 6px rgba(0,0,0, 0.25);
+          }
+
+          :host {
+            position: relative;
+          }
+
+          :host(.important) {
+            background: var(--color-primary, #ccc);
+            padding: 0.15rem;
+          }
+
+          :host-context(p) {
+            font-weight: bold;
+          }
+
+          ::slotted(.highlight) {
+            border-bottom: 2px red dotted;
+          }
+
+          .icon {
+            background-color: black;
+            color: white;
+            padding: 0.15rem 0.5rem;
+            text-align: center;
+            border-radius: 50%;
+          }
+        </style>
+        <slot>Some default</slot>
+        <span class="icon">?</span>
       `;
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
     }
-  
-    connectedCallback() {
-      if (this.hasAttribute('text')) {
-        this._tooltipText = this.getAttribute('text');
-      }
-      const tooltipIcon = this.shadowRoot.querySelector('span');
-      tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-      tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-      this.shadowRoot.appendChild(tooltipIcon);
-      this.style.position = 'relative';
+
+    this._tooltipIcon = this.shadowRoot.querySelector("span");
+    this._tooltipIcon.addEventListener(
+      "mouseenter",
+      this._showTooltip.bind(this)
+    );
+    this._tooltipIcon.addEventListener(
+      "mouseleave",
+      this._hideTooltip.bind(this)
+    );
+
+    this._render();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
     }
-  
-    _showTooltip() {
-      this._tooltipContainer = document.createElement('div');
-      this._tooltipContainer.textContent = this._tooltipText;
-      this.shadowRoot.appendChild(this._tooltipContainer);
-    }
-  
-    _hideTooltip() {
-      this.shadowRoot.removeChild(this._tooltipContainer);
+
+    if (name === "text") {
+      this._tooltipText = newValue;
     }
   }
-  
-  customElements.define('uc-tooltip', Tooltip);
-  
+
+  static get observedAttributes() {
+    return ["text"];
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener("mouseenter", this._showTooltip);
+    this._tooltipIcon.removeEventListener("mouseleave", this._hideTooltip);
+  }
+
+  _render() {
+    let tooltipContainer = this.shadowRoot.querySelector("div");
+
+    if (this._tooltipVisible) {
+      tooltipContainer = document.createElement("div");
+      tooltipContainer.textContent = this._tooltipText;
+      this.shadowRoot.appendChild(tooltipContainer);
+    } else {
+      if (tooltipContainer) {
+        this.shadowRoot.removeChild(tooltipContainer);
+      }
+    }
+  }
+
+  _showTooltip() {
+    this._tooltipVisible = true;
+    this._render();
+  }
+
+  _hideTooltip() {
+    this._tooltipVisible = false;
+    this._render();
+  }
+}
+
+customElements.define("uc-tooltip", Tooltip);
